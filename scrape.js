841e654a32c4a659d297914f7b42ec405eeb5211
Hasan Naseem <hn92@live.com>
2020-03-22 18:17:22 -0400

    add feature to figure out the average price based on ads fetched and recommends the lower prices options

--- scrape.js
+++ scrape.js
@@ -7,23 +7,46 @@ let options = {
 
 let params = {
     locationId: 1700272,  // Same as kijiji.locations.ONTARIO.TORONTO_GTA
-    categoryId: 0,  // Same as kijiji.categories.CARS_AND_VEHICLES
+    categoryId: 174,  // Same as kijiji.categories.CARS_AND_VEHICLES - 174, PS4-792, free stuff- 17220001
     sortByName: "dateDesc",  // Show the cheapest listings first
-    keywords: "ps4",
-    minPrice: 100,
-    maxPrice: 200,
-    adType: "OFFER"
+    keywords: "Audi A4 2013",
+    adType: "OFFER",
+    minPrice: 10,
+    maxPrice: 10000
     //adType: "WANTED"
 };
 
 // Scrape using returned promise
-kijiji.search(params, options).then(function(ads) {
+kijiji.search(params).then(function(ads) {
     // Use the ads array
+    let total = 0;
+    let totalPrice = 0;
     for (let i = 0; i < ads.length; ++i) {
-        console.log(ads[i].title);
+        //console.log(ads[i].title);
         //console.log(ads[i].description);
-        console.log(ads[i].attributes.price);
-        console.log(ads[i].url);
+        //console.log(ads[i].attributes.price);
+        //console.log(ads[i].url);
+        //console.log(`Price: ${ads[i].attributes.price}`)
+        //console.log(`Title: ${ads[i].title} -- Price: ${ads[i].attributes.price} -- date: ${ads[i].date} -- url: ${ads[i].url}`)
+        total++
+        totalPrice = totalPrice + ads[i].attributes.price
+    }
+    console.log("total ads fetched: " + total)
+    console.log(`Average Price for a ${params.keywords} is CAD$ ${Math.round(totalPrice/total)}`)
+    console.log("Check these Ads, they are below average price")
+    console.log("--------------------------------------------")
+    for (let i = 0; i < ads.length; ++i) {
+        //console.log(ads[i].title);
+        //console.log(ads[i].description);
+        //console.log(ads[i].attributes.price);
+        //console.log(ads[i].url);
+        if (ads[i].attributes.price < Math.round(totalPrice/total)) {
+          console.log("------------------------------------------")
+          console.log(`Title: ${ads[i].title}`)
+          console.log(`Price: ${ads[i].attributes.price}`)
+          console.log(`date: ${ads[i].date}`)
+          console.log(`url: ${ads[i].url}`)
+        }
     }
 }).catch(console.error);
 
